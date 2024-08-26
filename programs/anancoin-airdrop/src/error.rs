e4527b7dbf61a2cdc31c5528faacfaa65f45b0d3
帅进超 <shuaijinchao@gmail.com>
2024-08-27 00:02:11 +0800

    feat: support initialize contract

--- programs/anancoin-airdrop/src/error.rs
+++ programs/anancoin-airdrop/src/error.rs
@@ -5,6 +5,12 @@ pub enum ErrorCode {
     // 代币数量不足
     #[msg("token amount is not enough")]
     TokenAmountNotEnough,
+    // 领取代币超出限制
+    #[msg("claim token amount is out of limit")]
+    ClaimTokenAmountOutOfLimit,
+    // 领取代币为零
+    #[msg("claim token amount is zero")]
+    ClaimTokenAmountIsZero,
     // SOL 余额不足
     #[msg("gas fee is not enough")]
     GasFeeNotEnough,
--- programs/anancoin-airdrop/src/state.rs
+++ programs/anancoin-airdrop/src/state.rs
@@ -2,16 +2,12 @@
 use anchor_lang::prelude::*;
 
 #[account]
-pub struct Lemconn {
-    pub initialized: bool,
-    pub lemconn_owner_account: Pubkey,
-    pub lemconn_token_mint: Pubkey,
-    pub lemconn_token_account: Pubkey,
-    pub lemconn_token_fees: u64,
-    pub lemconn_token_mode: u8,
-    pub lemconn_token_decimal: u8,
-    pub pda_token_account: Pubkey,
-    pub pda_owner_bump: u8,
-    pub lemconn_fees_mint: Pubkey,
-    pub lemconn_fees_account: Pubkey,
+pub struct Config {
+    pub authority: Pubkey,
+    pub bump: u8,
+    pub token_mint: Pubkey,
+    pub token_account: Pubkey,
+    pub token_pda: Pubkey,
+    pub token_decimals: u8,
+    pub token_limit: u64,
 }
