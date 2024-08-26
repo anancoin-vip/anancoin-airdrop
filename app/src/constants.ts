ea8a25814687dd70a38e0af5acb319190292c4e0
帅进超 <shuaijinchao@gmail.com>
2024-08-26 23:06:43 +0800

    chore: remove debug code

--- app/src/constants.ts
+++
@@ -1,4 +0,0 @@
-import { web3 } from '@coral-xyz/anchor'
-
-export const lemconnTokenMint = new web3.PublicKey('43zGoQe2LkXmGJHnHjTWXW7SggQPaDiULhPnATWfXqc8')
-export const programId = new web3.PublicKey("sEs461DSnSm3m9ssbWZ6j8WXrWJm96owZsAgwVSVKqB")
